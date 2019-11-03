bc99371d574579d61d25ee618e4a1a4a322dc7af
Michal Grzelak <grzelakm@outlook.com>
2019-11-03 20:21:28 +0100

    Fix Python 2.X to 3.X compatibility issues

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-import mnist-loader
+import mnist_loader
+import network
+
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
+net = network.Network([784, 30, 10])
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
